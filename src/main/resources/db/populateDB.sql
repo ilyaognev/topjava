4f2908f7c385ff1eff5effa76bd104e551563e23
Ilya Ognev <ilya.fire@gmail.com>
2020-10-20 22:56:36 +0300

    HW3 01 Main part HW is done

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2020-04-12 04:05:06', 'Users night dozor', 1000),
+       (100000, '2020-04-12 12:05:06', 'Users lunch', 990),
+       (100000, '2020-04-12 16:05:06', 'Users second lunch dozor', 100),
+       (100001, '2020-04-12 10:05:06', 'Admin breakfast', 1000),
+       (100001, '2020-04-12 22:05:06', 'Admin dinner', 900);
