6949332986c19eeb1e18151b4bcf60b004598924
Ilya Ognev <ilya.fire@gmail.com>
2020-11-24 14:48:25 +0300

    HW07 01 - 1.1 and 1.2 is done

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -51,4 +51,16 @@ public class MealTo {
                 ", excess=" + excess +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        return id.equals(((MealTo) obj).id) || dateTime == ((MealTo) obj).dateTime || description.equals(((MealTo) obj).description) ||
+                calories == ((MealTo) obj).calories || excess == ((MealTo) obj).excess;
+    }
 }
