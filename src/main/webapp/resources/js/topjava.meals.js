0a536533eef2f2151e6af749069e5218849fb7d1
Ilya Ognev <ilya.fire@gmail.com>
2020-12-09 23:37:41 +0300

    HW09 04 removed seconds from view list of meals

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -28,7 +28,7 @@ $(function () {
                     "data": "dateTime",
                     "render": function (data, type, row) {
                         if (type === "display") {
-                            return data.replace("T", " ");
+                            return data.replace("T", " ").substring(0, 16);
                         }
                         return data;
                     }
